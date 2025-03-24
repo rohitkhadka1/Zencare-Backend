4dcf88e031d78d8ce16d177b3f2f8cacc5171b24
Rohit Khadka <rohitkhadka153@gmail.com>
2025-03-24 22:32:58 +0545

    Commit 5

--- Zencare/ZencareBackend/ZencareBackend/settings.py
+++ Zencare/ZencareBackend/ZencareBackend/settings.py
@@ -98,6 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     } if DEBUG else dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
         conn_max_age=600,
         conn_health_checks=True,
     )
