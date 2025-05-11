695c6aa522c454bbe1f516597d215f66250595bc
Rohit Khadka <rohitkhadka153@gmail.com>
2025-05-11 12:27:34 +0545

    fix: database url

--- zencare/settings.py
+++ zencare/settings.py
@@ -134,12 +134,19 @@ WSGI_APPLICATION = 'zencare.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.sqlite3',
+#     #     'NAME': BASE_DIR / 'db.sqlite3',
+#     # } if DEBUG else 
+#     dj_database_url.config(
+#         default=os.getenv('DATABASE_URL'),
+#         conn_max_age=600,
+#         conn_health_checks=True,
+#     )
+# }
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # } if DEBUG else 
-    dj_database_url.config(
+    'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL'),
         conn_max_age=600,
         conn_health_checks=True,
